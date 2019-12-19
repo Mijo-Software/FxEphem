53450064eca1c9fa4299db0dcb905574d87382c9
Michael Johne <mjohne@users.noreply.github.com>
2019-12-19 12:14:26 +0100

    Add files via upload

---
+++ FxEphem/MainForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FxEphem
+{
+  public partial class MainForm : Form
+  {
+    public MainForm() => InitializeComponent();
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      MessageBox.Show("JD: " + FxEphemClass.CalcJulianDate().ToString());
+      MessageBox.Show("MJD: " + FxEphemClass.CalcModifiedJulianDate().ToString());
+      MessageBox.Show("Centuries since 2000: " + FxEphemClass.CalcCenturiesSince2000().ToString());
+    }
+  }
+}
